abe989e31450e18c11be00720726cd48fe6c49d5
Cuenta Personal <estaniolmedo@gmail.com>
2025-09-07 17:26:20 -0300

    Implement ProductSearch component with styled input and button

--- src/components/products/product_search/product_search.tsx
+++ src/components/products/product_search/product_search.tsx
@@ -1,3 +1,19 @@
+"use client";
+import {Search} from "lucide-react";
+
+import style from "./product_search.module.css";
+
 export default function ProductSearch() {
-  return <div>ProductSearch</div>;
+  return (
+    <div className="relative box-border flex w-full max-w-md items-center">
+      <input
+        className={style.search_input}
+        placeholder="Buscar productos por nombre o marca"
+        type="text"
+      />
+      <button className={style.search_btn} type="button">
+        <Search size={20} />
+      </button>
+    </div>
+  );
 }
