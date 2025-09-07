f333d4bb872551d55c68c41c8374112db9171105
Cuenta Personal <estaniolmedo@gmail.com>
2025-09-07 19:46:11 -0300

    Add use-debounce and zustand dependencies; update ProductSearch component with debounced search functionality and style adjustments

--- src/components/products/product_search/product_search.tsx
+++ src/components/products/product_search/product_search.tsx
@@ -1,15 +1,25 @@
 "use client";
 import {Search} from "lucide-react";
+import {useDebouncedCallback} from "use-debounce";
 
 import style from "./product_search.module.css";
 
+import {useProductsStore} from "@/store/product.store";
+
 export default function ProductSearch() {
+  const {setQuery} = useProductsStore();
+
+  const handleSearch = useDebouncedCallback((value: string) => {
+    setQuery(value);
+  }, 500);
+
   return (
     <div className="relative box-border flex w-full max-w-md items-center">
       <input
         className={style.search_input}
         placeholder="Buscar productos por nombre o marca"
         type="text"
+        onChange={(e) => handleSearch(e.target.value)}
       />
       <button className={style.search_btn} type="button">
         <Search size={20} />
