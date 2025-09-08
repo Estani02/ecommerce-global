653789c7eb707b7609248ea6fcfdbfd821a114a3
Cuenta Personal <estaniolmedo@gmail.com>
2025-09-08 01:41:10 -0300

    Refactor ProductByIdPage to enhance layout and structure with main content sections

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,3 +1,12 @@
 export default function ProductByIdPage() {
-  return <div>Product By ID Page</div>;
+  return (
+    <main className="flex min-h-screen w-full items-center gap-16 p-4 pt-24 md:p-24">
+      <div className="h-[600px] flex-1 bg-amber-300">
+        <h1>Product Details</h1>
+      </div>
+      <div className="h-[600px] flex-1 rounded-[1.4rem] rounded-tl-[1.4rem] bg-amber-200 p-8">
+        <p>More content here...</p>
+      </div>
+    </main>
+  );
 }
