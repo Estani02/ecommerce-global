b7bfa4bcd1a1230c3ede987237ee86efc0cfe5be
Cuenta Personal <estaniolmedo@gmail.com>
2025-09-08 01:19:28 -0300

    Add 404 error SVG, update page padding, modify search button styles, and enhance product JSON structure
    - Added a new SVG file for 404 error page.
    - Adjusted padding in the main page layout for better spacing.
    - Updated search button styles to improve user experience.
    - Enhanced product JSON structure by ensuring all products have consistent attributes and added missing fields.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import ProductList from "@/components/products/product_list/product_list";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center gap-16 p-12 md:p-24">
+    <main className="flex min-h-screen flex-col items-center gap-16 p-4 pt-24 md:p-24">
       <div>
         <h1 className="text-center text-4xl font-bold">Bienvenido a Global Commerce</h1>
         <p className="mt-4 text-center text-lg text-gray-600">
