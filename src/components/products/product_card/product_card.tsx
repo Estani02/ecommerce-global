849167f7ede049b1ff104a10e23f2af423dbdba4
Cuenta Personal <estaniolmedo@gmail.com>
2025-09-08 01:19:19 -0300

    Update ProductCard to handle favorite toggle; pass mutate function from ProductList

--- src/components/products/product_card/product_card.tsx
+++ src/components/products/product_card/product_card.tsx
@@ -10,26 +10,54 @@ import {Product} from "@/types/product";
 
 interface ProductCardProps {
   product: Product;
+  mutate: () => void;
 }
 
-export default function ProductCard({product}: ProductCardProps) {
+export default function ProductCard({product, mutate}: ProductCardProps) {
+  const handleFavoriteToggle = async () => {
+    mutate(
+      async (products: Product[] = []) => {
+        return products.map((p) => (p.id === product.id ? {...p, fav: !product.fav} : p));
+      },
+      {optimisticData: undefined, rollbackOnError: true, revalidate: false},
+    );
+
+    const response = await fetch(`/api/product`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        fav: !product.fav,
+        id: product.id,
+      }),
+    });
+
+    if (!response.ok) {
+      console.error("Failed to update favorite status");
+    } else {
+      mutate();
+    }
+  };
+
   return (
-    <Link href={`/product/${product.id}`}>
-      <div className={styles.card}>
+    <div className={styles.card}>
+      <Link href={`/product/${product.id}`}>
         <div className={styles.image_container}>
           <Image alt="Product Image" height={100} src={product.imagen} width={100} />
 
           <div className={styles.price}>${product.precio}</div>
         </div>
+      </Link>
 
-        <label className={styles.favorite}>
-          <span className="hidden">Mark as favorite</span>
-          <input checked type="checkbox" />
-          <svg fill="#000000" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path d="M12 20a1 1 0 0 1-.437-.1C11.214 19.73 3 15.671 3 9a5 5 0 0 1 8.535-3.536l.465.465.465-.465A5 5 0 0 1 21 9c0 6.646-8.212 10.728-8.562 10.9A1 1 0 0 1 12 20z" />
-          </svg>
-        </label>
-
+      <label className={styles.favorite}>
+        <span className="hidden">Mark as favorite</span>
+        <input checked={product?.fav} type="checkbox" onChange={handleFavoriteToggle} />
+        <svg fill="#000000" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path d="M12 20a1 1 0 0 1-.437-.1C11.214 19.73 3 15.671 3 9a5 5 0 0 1 8.535-3.536l.465.465.465-.465A5 5 0 0 1 21 9c0 6.646-8.212 10.728-8.562 10.9A1 1 0 0 1 12 20z" />
+        </svg>
+      </label>
+      <Link href={`/product/${product.id}`}>
         <div className={styles.content}>
           <div className={styles.brand}>{product.marca}</div>
           <div className={styles.product_name}>{product.titulo}</div>
@@ -48,14 +76,14 @@ export default function ProductCard({product}: ProductCardProps) {
         <div className={styles.content_product_summary}>
           <p className={styles.product_summary}>{product.descripcion}</p>
         </div>
+      </Link>
 
-        <div className={styles.button_container}>
-          <button className={`${styles.buy_button} ${styles.button}`}>Buy Now</button>
-          <button className={`${styles.cart_button} ${styles.button}`}>
-            <ShoppingCart />
-          </button>
-        </div>
+      <div className={styles.button_container}>
+        <button className={`${styles.buy_button} ${styles.button}`}>Buy Now</button>
+        <button className={`${styles.cart_button} ${styles.button}`}>
+          <ShoppingCart />
+        </button>
       </div>
-    </Link>
+    </div>
   );
 }
--- src/components/products/product_list/product_list.tsx
+++ src/components/products/product_list/product_list.tsx
@@ -1,5 +1,8 @@
 /* eslint-disable react/jsx-no-useless-fragment */
 "use client";
+
+import Image from "next/image";
+
 import ProductCard from "../product_card/product_card";
 import ProductSearch from "../product_search/product_search";
 
@@ -10,7 +13,7 @@ import {useProducts} from "@/hooks/useProducts";
 
 export default function ProductList() {
   const {query} = useProductsStore();
-  const {products, isLoading} = useProducts(query);
+  const {products, isLoading, mutate} = useProducts(query);
 
   return (
     <div className="grid w-full max-w-[1300px] grid-cols-1 place-items-center justify-between gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
@@ -19,7 +22,7 @@ export default function ProductList() {
         {isLoading ? (
           <div className={`${styles.skeleton_block} pulsate`} />
         ) : (
-          <p>{products?.length} productos encontrados</p>
+          <p className="whitespace-nowrap">{products?.length} productos encontrados</p>
         )}
       </div>
       {isLoading ? (
@@ -28,10 +31,27 @@ export default function ProductList() {
             <div key={index} className={`${styles.skeleton_card} pulsate`} />
           ))}
         </>
+      ) : products?.length === 0 ? (
+        <div className="col-span-full flex w-full flex-col items-center justify-center text-center">
+          <Image
+            alt="404 Not Found"
+            className="size-[246px]"
+            height={56}
+            src="/404-error.svg"
+            width={56}
+          />
+          <div className="text-sm md:text-base">
+            <h2 className="mb-4 text-3xl font-bold">No se encontraron productos</h2>
+            <p>No hay productos que coincidan con tu búsqueda.</p>
+            <p>
+              Intenta ajustar tus filtros o verifica que el nombre que estás buscando sea correcto.
+            </p>
+          </div>
+        </div>
       ) : (
         <>
           {products?.map((product) => (
-            <ProductCard key={product.id} product={product} />
+            <ProductCard key={product.id} mutate={mutate} product={product} />
           ))}
         </>
       )}
